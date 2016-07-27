e68f7edc1bede0ca5f92035a3f45628a88505457
Kalen Peterson <kpeterson@forsythe.com>
2016-07-27 10:31:51 -0500

    Fixed issue with fact name

--- lib/facter/git_version.rb
+++ lib/facter/git_version.rb
@@ -1,4 +1,4 @@
-Facter.add(:git_version) do
+Facter.add('git_version') do
   confine :osfamily => 'Linux'
   setcode do
     Facter::Core::Execution.exec('/usr/bin/git --version |/usr/bin/awk \'{print $3}\'')
